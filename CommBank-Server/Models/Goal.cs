15699d3bc4ca8f3448eb9f8344bc570a486187d4
zshaikh8963-lab <zshaikh8963-lab@users.noreply.github.com>
2026-08-05 21:47:30 +0530

    Add optional Icon field to Goal model and update target framework to net10.0

--- CommBank-Server/Models/Goal.cs
+++ CommBank-Server/Models/Goal.cs
@@ -1,4 +1,4 @@
-﻿using MongoDB.Bson;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace CommBank.Models;
@@ -27,4 +27,6 @@ public class Goal
 
     [BsonRepresentation(BsonType.ObjectId)]
     public string? UserId { get; set; }
+
+    public string? Icon { get; set; }
 }
